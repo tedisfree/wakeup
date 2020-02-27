92d4d4ff175c116b2eb65d1d3643d37a6645da82
ted <tykim2@canon-bs.co.kr>
2020-02-27 11:01:26 +0900

    Init commit

---
+++ wakeup/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using wakeup.Properties;
+
+namespace wakeup
+{
+    static class Program
+    {
+        /// <summary>
+        /// 해당 애플리케이션의 주 진입점입니다.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Form1());
+            Application.Run(new MyCustormApplicationContext());
+        }
+
+
+    }
+
+    public class MyCustormApplicationContext : ApplicationContext
+    {
+        [DllImport("user32.dll")]
+        static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint dwData, int dwExtraInfo);
+
+        private NotifyIcon trayIcon;
+
+        public MyCustormApplicationContext()
+        {
+            trayIcon = new NotifyIcon()
+            {
+                Icon = Resources.AppIcon,
+                ContextMenu = new ContextMenu(new MenuItem[]
+                {
+                    new MenuItem("Exit", Exit)
+                }),
+                Visible = true
+            };
+
+            trayIcon.MouseDoubleClick += trayIconMouseDoubleClickEventHandler;
+            run();
+        }
+
+        private void run()
+        {
+            Debug.WriteLine("thread runs");
+            new Thread(new ThreadStart(exeStateNotifier)).Start();
+        }
+
+        private void keepMoving()
+        {
+            while (true)
+            {
+                int curx = Cursor.Position.X;
+                int cury = Cursor.Position.Y;
+
+                Debug.WriteLine("Hello x=" + curx + " y=" + cury);
+                Cursor.Position = new Point(curx + 100, cury + 100);
+                Thread.Sleep(100);
+                Cursor.Position = new Point(curx + 1, cury + 1);
+
+                Thread.Sleep(5000);
+            }
+        }
+
+
+        private void exeStateNotifier()
+        {
+            while (true)
+            {
+                Debug.WriteLine("wakeup!");
+                NativeMethods.SetThreadExecutionState(EXECUTION_STATE.ES_DISPLAY_REQUIRED);
+                Thread.Sleep(5000);
+            }
+
+
+        }
+        public enum EXECUTION_STATE : uint
+        {
+            ES_AWAYMODE_REQUIRED = 0x00000040,
+            ES_CONTINUOUS = 0x80000000,
+            ES_DISPLAY_REQUIRED = 0x00000002,
+        }
+
+
+        internal class NativeMethods
+        {
+            [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+            public static extern EXECUTION_STATE SetThreadExecutionState(EXECUTION_STATE esFlags);
+        }
+
+        private void trayIconMouseDoubleClickEventHandler(object sender, EventArgs e)
+        {
+            Debug.WriteLine("double clicked!");
+        }
+
+        private void Exit(object sender, EventArgs e)
+        {
+            trayIcon.Visible = false;
+            Application.Exit();
+        }
+
+    }
+}
